a35316e3208d8771e6a4d5699ec18a3dbd02269b
TheSpiralArt <thespiralart484@gmail.com>
2024-09-12 21:34:26 +0800

    Update GlRenderbuffer.java

--- src/main/java/net/vulkanmod/gl/GlRenderbuffer.java
+++ src/main/java/net/vulkanmod/gl/GlRenderbuffer.java
@@ -64,9 +64,9 @@ public class GlRenderbuffer {
 
         switch (pName) {
             case GL30.GL_TEXTURE_MAX_LEVEL -> bound.setMaxLevel(param);
-            case GL30.GL_TEXTURE_MAX_LOD -> {};
-            case GL30.GL_TEXTURE_MIN_LOD -> {};
-            case GL30.GL_TEXTURE_LOD_BIAS -> {};
+            case GL30.GL_TEXTURE_MAX_LOD -> {}
+            case GL30.GL_TEXTURE_MIN_LOD -> {}
+            case GL30.GL_TEXTURE_LOD_BIAS -> {}
 
             case GL11.GL_TEXTURE_MAG_FILTER -> bound.setMagFilter(param);
             case GL11.GL_TEXTURE_MIN_FILTER -> bound.setMinFilter(param);
