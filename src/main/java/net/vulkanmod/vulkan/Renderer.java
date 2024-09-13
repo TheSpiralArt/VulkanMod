f11de1899e79df0dbd2f11ee7b97e425ba18a1d9
TheSpiralArt <thespiralart484@gmail.com>
2024-09-14 07:43:18 +0800

    Update Renderer.java

--- src/main/java/net/vulkanmod/vulkan/Renderer.java
+++ src/main/java/net/vulkanmod/vulkan/Renderer.java
@@ -686,7 +686,7 @@ public class Renderer {
             viewport.x(0.0f);
             viewport.y(height);
             viewport.width(width);
-            viewport.height(-height);
+            viewport.height(height);
             viewport.minDepth(0.0f);
             viewport.maxDepth(1.0f);
 
