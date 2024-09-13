3e5afc7f0a7903fe0093f2ea35d7c515d81f7590
TheSpiralArt <thespiralart484@gmail.com>
2024-09-13 20:32:18 +0800

    Update Renderer.java

--- src/main/java/net/vulkanmod/vulkan/Renderer.java
+++ src/main/java/net/vulkanmod/vulkan/Renderer.java
@@ -613,41 +613,40 @@ public class Renderer {
 
     private static VkOffset2D transformToOffset(VkOffset2D offset2D, int x, int y, int w, int h) {
         int pretransformFlags = Vulkan.getPretransformFlags();
-        if(pretransformFlags == 0) {
-            offset2D.set(x, y);
-            return offset2D;
+    
+        if (pretransformFlags == 0) {
+            return offset2D.set(x, y);
         }
+
         Framebuffer boundFramebuffer = INSTANCE.boundFramebuffer;
         int framebufferWidth = boundFramebuffer.getWidth();
         int framebufferHeight = boundFramebuffer.getHeight();
+
         switch (pretransformFlags) {
             case VK_SURFACE_TRANSFORM_ROTATE_90_BIT_KHR -> {
-                offset2D.x(framebufferWidth - h - y);
-                offset2D.y(x);
+                offset2D.x = framebufferWidth - h - y;
+                offset2D.y = x;
             }
             case VK_SURFACE_TRANSFORM_ROTATE_180_BIT_KHR -> {
-                offset2D.x(framebufferWidth - w - x);
-                offset2D.y(framebufferHeight - h - y);
+                offset2D.x = framebufferWidth - w - x;
+                offset2D.y = framebufferHeight - h - y;
             }
             case VK_SURFACE_TRANSFORM_ROTATE_270_BIT_KHR -> {
-                offset2D.x(y);
-                offset2D.y(framebufferHeight - w - x);
+                offset2D.x = y;
+                offset2D.y = framebufferHeight - w - x;
             }
             default -> {
-                offset2D.x(x);
-                offset2D.y(y);
+                offset2D.x = x;
+                offset2D.y = y;
             }
         }
+    
         return offset2D;
     }
 
     private static VkExtent2D transformToExtent(VkExtent2D extent2D, int w, int h) {
         int pretransformFlags = Vulkan.getPretransformFlags();
-        if(pretransformFlags == VK_SURFACE_TRANSFORM_ROTATE_90_BIT_KHR ||
-                pretransformFlags == VK_SURFACE_TRANSFORM_ROTATE_270_BIT_KHR) {
-            return extent2D.set(h, w);
-        }
-        return extent2D.set(w, h);
+        return pretransformFlags == VK_SURFACE_TRANSFORM_ROTATE_90_BIT_KHR || pretransformFlags == VK_SURFACE_TRANSFORM_ROTATE_270_BIT_KHR ? extent2D.set(h, w) : extent2D.set(w, h);
     }
     
     public static void setViewport(int x, int y, int width, int height) {
