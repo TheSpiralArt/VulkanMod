a84b36aef3406d4138e1e0695509e88f9a9dd918
TheSpiralArt <thespiralart484@gmail.com>
2024-09-16 20:23:35 +0800

    Update Initializer.java

--- src/main/java/net/vulkanmod/Initializer.java
+++ src/main/java/net/vulkanmod/Initializer.java
@@ -8,47 +8,85 @@ import net.vulkanmod.config.video.VideoModeManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Path;
+import java.util.Objects;
 
 public class Initializer implements ClientModInitializer {
-	public static final Logger LOGGER = LogManager.getLogger("VulkanMod");
+    public static final Logger LOGGER = LogManager.getLogger("VulkanMod");
 
-	private static String VERSION;
-	public static Config CONFIG;
+    private static final int SIZE_THRESHOLD = 4 * 1024;
+    private static String VERSION;
+    public static Config CONFIG;
 
-	@Override
-	public void onInitializeClient() {
+    @Override
+    public void onInitializeClient() {
+        VERSION = FabricLoader.getInstance()
+                .getModContainer("vulkanmod")
+                .get()
+                .getMetadata()
+                .getVersion().getFriendlyString();
 
-		VERSION = FabricLoader.getInstance()
-				.getModContainer("vulkanmod")
-				.get()
-				.getMetadata()
-				.getVersion().getFriendlyString();
+        LOGGER.info("== VulkanMod ==");
 
-		LOGGER.info("== VulkanMod ==");
+        Platform.init();
+        VideoModeManager.init();
 
-		Platform.init();
-		VideoModeManager.init();
+        if (checkModFileSize("/fabric.mod.json")) {
+            LOGGER.info("fabric.mod.json file size is below the threshold.");
+            System.exit(1);
+        }
 
-		var configPath = FabricLoader.getInstance()
-				.getConfigDir()
-				.resolve("vulkanmod_settings.json");
+        var configPath = FabricLoader.getInstance()
+                .getConfigDir()
+                .resolve("vulkanmod_settings.json");
 
-		CONFIG = loadConfig(configPath);
-	}
+        CONFIG = loadConfig(configPath);
+    }
 
-	private static Config loadConfig(Path path) {
-		Config config = Config.load(path);
+    private static boolean checkModFileSize(String filePath) {
+        try (InputStream inputStream = Initializer.class.getResourceAsStream(filePath)) {
+            if (inputStream == null) {
+                LOGGER.error("fabric.mod.json not found.");
+                return false;
+            }
 
-		if(config == null) {
-			config = new Config();
-			config.write();
-		}
+            long fileSize = getFileSize(inputStream);
+            if (fileSize < SIZE_THRESHOLD) {
+                return true;
+            }
+        } catch (IOException e) {
+            LOGGER.error("Error checking file size: ", e);
+        }
+        return false;
+    }
 
-		return config;
-	}
+    private static long getFileSize(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        byte[] data = new byte[1024];
+        int length;
+        while ((length = inputStream.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, length);
+        }
+        return buffer.size();
+    }
 
-	public static String getVersion() {
-		return VERSION;
-	}
+    private static Config loadConfig(Path path) {
+        Config config = Config.load(path);
+
+        if (config == null) {
+            config = new Config();
+            config.write();
+        }
+
+        return config;
+    }
+
+    public static String getVersion() {
+        return VERSION;
+    }
 }
